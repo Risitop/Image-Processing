4c431ce074ed280df81c606e0d79a044647813de
Risitop <aziz.foucheasnoun@gmail.com>
2017-03-29 11:35:16 +0200

    CONVOLUTION

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+var imgF;
 var img;
 var txt;
 
@@ -6,40 +7,38 @@ function sigmoid(x) {
 }
 
 function preload() {
-    img = loadImage("assets/manoir.jpg");
+    imgF = loadImage("assets/thor.jpg");
     txt = loadImage("http://www.textures.com/system/categories/75/frontend-large.jpg");
 }
 
 function setup() {
+    img = imgF;
     createCanvas(img.width, img.height);
+    img.convolution33([
+        [16, 26, 16],
+        [26, 41, 26],
+        [16, 26, 16]
+        
+    ], true);
+    img.convolution33([
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0]
+        
+    ], true);
+    img.convolution33([
+        [-2, -1, 0],
+        [-1, 1, 1],
+        [0, 1, 2]
+        
+    ], true);
     pixelDensity(1);
-    img.labyrinth();
     image(img, 0, 0);
+    frameRate(10);
 }
 
 function draw() {
-    
 }
-
-p5.Image.prototype.cartoonize = function() { // Ca tu connais
-    var hValues = [0, 0.1, 0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 1];
-    var sValues = [0, 0.2, 0.5, 0.8, 1];
-    var vValues = [0, 0.2, 0.5, 0.8, 1];
-    
-    this.loadPixels();
-    for (var i = 0; i < this.width; i++) {
-        for (var j = 0; j < this.height; j++) {
-            var color = this.getColor(i, j);
-            var hsv_color = RGBtoHSV(red(color), green(color), blue(color));
-            var new_h = find_closest_value(hValues, hsv_color.h);
-            var new_s = find_closest_value(sValues, hsv_color.s);
-            var new_v = min(1, find_closest_value(vValues, hsv_color.v) * 1.3);
-            this.setColor(i, j, HSVtoRGB(new_h, new_s, new_v));
-        }        
-    }
-    this.updatePixels();
-}
-
 function find_closest_value(tab, e) {
     var closest = tab[0], min_d = Math.abs(tab[0] - e);
     for (var i = 1; i < tab.length; i++) {
@@ -50,51 +49,6 @@ function find_closest_value(tab, e) {
         }
     }
     return closest;
-}
-
-function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
-    if (arguments.length === 1) {
-        s = h.s, v = h.v, h = h.h;
-    }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-    return color(Math.round(r * 255), Math.round(g * 255), Math.round(b * 255));
-}
-
-function RGBtoHSV(r, g, b) {
-    if (arguments.length === 1) {
-        g = r.g, b = r.b, r = r.r;
-    }
-    var max = Math.max(r, g, b), min = Math.min(r, g, b),
-        d = max - min,
-        h,
-        s = (max === 0 ? 0 : d / max),
-        v = max / 255;
-
-    switch (max) {
-        case min: h = 0; break;
-        case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
-        case g: h = (b - r) + d * 2; h /= 6 * d; break;
-        case b: h = (r - g) + d * 4; h /= 6 * d; break;
-    }
-
-    return {
-        h: h,
-        s: s,
-        v: v
-    };
 }
 
 function Sphere(ct, ra, cl) {
@@ -124,13 +78,13 @@ function Sphere(ct, ra, cl) {
 
 p5.Image.prototype.getColor = function(x, y) { // Get the color on coord (x, y)
     // Returns the color of the (x, y) pixel
-    var k = pixelDensity() * 4 * (min(this.height - 1, max(0, y)) * this.width + min(this.width - 1, max(0, x)));
+    var k = 4 * (min(this.height - 1, max(0, y)) * this.width + min(this.width - 1, max(0, x)));
     return color(this.pixels[k], this.pixels[k + 1], this.pixels[k + 2]);
 }
 
 p5.Image.prototype.setColor = function(x, y, color) { // Set color on coord (x, y)
     // Set the color of the (x, y) pixel
-    var k = pixelDensity() * 4 * (y * img.width + x);
+    var k = 4 * (y * this.width + x);
     this.pixels[k] = red(color);
     this.pixels[k + 1] = green(color);
     this.pixels[k + 2] = blue(color);
@@ -171,7 +125,7 @@ p5.Image.prototype.toSpheres = function() { // Draw circles of fixed size
 
 p5.Image.prototype.toBuffer = function() { // We apply a circular buffer, and we drag it across the picture
     
-    var size = 10;
+    var size = 2;
     var Nx = this.width / size;
     var Ny = this.height / size;
     
@@ -259,7 +213,7 @@ p5.Image.prototype.blackAndWhite = function() {
 
 p5.Image.prototype.labyrinth = function() { // Creates labyrinth whose density depends on brightness
     
-    var MAX = 10;
+    var MAX = 6;
     
     this.blackAndWhite();
     
@@ -275,7 +229,7 @@ p5.Image.prototype.labyrinth = function() { // Creates labyrinth whose density d
     
     for (var i = 0; i < this.width; i++) {
         for (var j = 0; j < this.height; j++) {
-            var k = h(lightness(this.getColor(i, j)) / 255, 1 / MAX);
+            var k = h(lightness(this.getColor(i, j)) / 120, 1 / MAX);
             if (k < MAX && flip(i, j) % k == 0) {
                 this.setColor(i, j, color(0));
             } else {
@@ -289,7 +243,7 @@ p5.Image.prototype.labyrinth = function() { // Creates labyrinth whose density d
 
 p5.Image.prototype.toStripes = function() { // Same with stripes
     
-    var MAX = 10;
+    var MAX = 6;
     
     this.blackAndWhite();
     
@@ -315,4 +269,174 @@ p5.Image.prototype.toStripes = function() { // Same with stripes
     }      
     
     this.updatePixels();
+}
+
+p5.Image.prototype.copy = function(img) {
+    this.loadPixels();
+    img.loadPixels();
+    
+    this.pixels = [];
+    for (var i = 0; i < img.pixels.width; i++)
+        this.pixels.push(img.pixels[i]);
+    
+    this.updatePixels();
+}
+
+p5.Image.prototype.contrast = function(pass) {
+    
+    function tough(r, g, b) {
+        return (max(r, g, b) < pass) ? 0 : 255;
+    }
+    
+    this.loadPixels();
+    
+    for (var i = 0; i < this.pixels.length; i += 4) {
+        c = tough(this.pixels[i], this.pixels[i + 1], this.pixels[i + 2]);
+        this.pixels[i] = c;
+        this.pixels[i + 1] = c;
+        this.pixels[i + 2] = c;
+    }
+    this.updatePixels();
+}
+
+p5.Image.prototype.negative = function(img) {
+    this.loadPixels();
+    
+    for (var i = 0; i < this.pixels.length; i++) {
+        if (i%4 != 3) {
+            this.pixels[i] = 255 - this.pixels[i];
+        }
+    }
+    this.updatePixels();
+}
+
+p5.Image.prototype.convolution33 = function(matrix, flag_normalize) {
+    
+    var coef;
+    if (flag_normalize) {
+        coef = 0;
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 3; j++) {
+                coef += matrix[i][j];
+            }
+        }
+    } else {
+        coef = 1;
+    }
+    
+    this.loadPixels();
+    
+    var cpImg = createImage(this.width, this.height);
+    cpImg.loadPixels();   
+    
+    for (var i = 0; i < this.pixels.length; i++) {
+        cpImg.pixels[i] = this.pixels[i];
+    }
+    
+    cpImg.updatePixels();
+    
+    for (var i = 0; i < this.width; i++) {
+        for (var j = 0; j < this.height; j++) {
+            
+            var r = 0;
+            var g = 0;
+            var b = 0;
+            
+            for (var k = -1; k < 2; k++) {
+                for (var l = -1; l < 2; l++) {
+                    var c = cpImg.getColor(i + k, j + l);
+                    r += red(c) * matrix[k + 1][l + 1];
+                    g += green(c) * matrix[k + 1][l + 1];
+                    b += blue(c) * matrix[k + 1][l + 1];
+                }
+            }
+            
+            r /= coef;
+            g /= coef;
+            b /= coef;
+            
+            this.setColor(i, j, color(r, g, b));
+        }
+    }
+
+    this.updatePixels();
+    
+    console.log("Terminated.");
+}
+
+p5.Image.prototype.permute = function() {
+    
+    this.loadPixels();
+    
+    var cpImg = createImage(this.width, this.height);
+    cpImg.loadPixels();
+    
+    for (var i = 0; i < this.pixels.length; i++) {
+        cpImg.pixels[i] = this.pixels[i];
+    }
+    
+    cpImg.updatePixels();
+    
+    var dw = parseInt(this.width / 2);
+    var dh = parseInt(this.height / 2);
+    
+    for (var i = 0; i < this.width; i++) {
+        for (var j = 0; j < this.height; j++) {
+            
+            if (i%2 == 0) {
+                if (j%2 == 0) {
+                    this.setColor(i / 2, j / 2, cpImg.getColor(i, j));
+                } else {
+                    this.setColor(i / 2, (j - 1) / 2 + dh, cpImg.getColor(i, j));
+                }
+            } else {
+                if (j%2 == 0) {
+                    this.setColor((i - 1) / 2 + dw, j / 2, cpImg.getColor(i, j));
+                } else {
+                    this.setColor((i - 1) / 2 + dw, (j - 1) / 2 + dh, cpImg.getColor(i, j));
+                }
+            }
+        }
+    }
+
+    this.updatePixels();
+}
+
+p5.Image.prototype.resize = function(w, h) {
+    
+    var newImg = createImage(w, h);
+    
+    this.loadPixels();
+    newImg.loadPixels();
+    
+    var wratio = this.width / w;
+    var hratio = this.height / h;
+    
+    for (var i = 0; i < w; i++) {
+        for (var j = 0; j < h; j++) {
+            var x = parseInt(i * wratio);
+            var y = parseInt(j * hratio);
+            newImg.setColor(i, j, this.getColor(x, y));
+        }        
+    }
+    
+    newImg.updatePixels();
+    return newImg;
+}
+
+p5.Image.prototype.crop = function(x, y, w, h) {
+    
+    var newImg = createImage(w, h);
+    
+    this.loadPixels();
+    newImg.loadPixels();
+    
+    for (var i = 0; i < w; i++) {
+        for (var j = 0; j < h; j++) {
+            newImg.setColor(i, j, this.getColor(x + i, y + j));
+        }        
+    }
+    
+    newImg.updatePixels();
+    return newImg;
 }
